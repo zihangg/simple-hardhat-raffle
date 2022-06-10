f135994898c5dc49e299f88a420812ce60f5e7ae
unknown <kevu.lee@gmail.com>
2022-06-10 18:00:02 +0800

    init

---
+++ test/staging/Raffle.staging.test.js
@@ -0,0 +1,53 @@
+const { getNamedAccounts, deployments, ethers, network } = require("hardhat")
+const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
+const { assert, expect } = require("chai")
+const { isCallTrace } = require("hardhat/internal/hardhat-network/stack-traces/message-trace")
+
+developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Raffle", function () {
+          let raffle, raffleEntranceFee, deployer
+
+          beforeEach(async function () {
+              deployer = (await getNamedAccounts()).deployer
+              raffle = await ethers.getContract("Raffle", deployer)
+              raffleEntranceFee = await raffle.getEntranceFee()
+          })
+
+          describe("fulfillRandomWords", function () {
+              it("works with live Chainlink Keepers and Chainlink VRF, we get a winner", async function () {
+                  // enter the raffle
+                  console.log(`running test with contract address at ${raffle.address}`)
+                  const startingTimeStamp = await raffle.getLatestTimeStamp()
+                  accounts = await ethers.getSigners()
+
+                  await new Promise(async (resolve, reject) => {
+                      raffle.once("WinnerPicked", async () => {
+                          console.log("WinnerPicked event fired!")
+                          try {
+                              const recentWinner = await raffle.getRecentWinner()
+                              const raffleState = await raffle.getRaffleState()
+                              const winnerEndingBalance = await accounts[0].getBalance()
+                              const endingTimeStamp = await raffle.getLatestTimeStamp()
+
+                              await expect(raffle.getPlayer(0)).to.be.reverted
+                              assert.equal(recentWinner.toString(), accounts[0].address)
+                              assert.equal(raffleState.toString(), "0")
+                              assert.equal(
+                                  winnerEndingBalance.toString(),
+                                  winnerStartingBalance.add(raffleEntranceFee).toString()
+                              )
+                              assert(endingTimeStamp > startingTimeStamp)
+                          } catch (e) {
+                              console.log(e)
+                              reject(e)
+                          }
+                          resolve()
+                      })
+
+                      await raffle.enterRaffle({ value: raffleEntranceFee })
+                      const winnerStartingBalance = await accounts[0].getBalance()
+                  })
+              })
+          })
+      })
